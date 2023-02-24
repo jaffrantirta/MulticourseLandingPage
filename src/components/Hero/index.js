3bd3271ec4bfb53760ffae061b405795fb88fe98
jaffrantirta <franartika@gmail.com>
2023-02-24 15:44:29 +0800

    navbar responsive

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,10 +3,10 @@ import ButtonRounded from '../ButtonRounded'
 
 export default function Hero() {
     return (
-        <div className={`h-screen relative bg-black flex flex-col items-center justify-center font-primary text-center text-white`}>
+        <div id='hero-section' className={`h-screen relative bg-black flex flex-col items-center justify-center font-primary text-center text-white`}>
             <div className="relative z-10 px-10">
-                <h1 className='text-4xl md:text-7xl mb-5'>Belajar berbagai<br></br>macam bersama</h1>
-                <p className='text-lg md:text-xl mb-5'>Kursus Bahasa Inggris, Ngoding, Desain Grafis atau Matematika</p>
+                <h1 className='text-2xl md:text-7xl mb-5'>Belajar berbagai<br></br>macam bersama</h1>
+                <p className='text-sm md:text-xl mb-5'>Kursus Bahasa Inggris, Ngoding, Desain Grafis atau Matematika</p>
                 <div className='gap-5 flex flex-col md:flex-row justify-center items-center'>
                     <ButtonRounded text={`Daftar sekarang!`} />
                     <ButtonRounded text={`Daftar sekarang!`} isBorderOnly={true} />
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ImgLogo } from '../../assets/images'
 import ButtonRounded from '../ButtonRounded'
 
 export default function Navbar({ className }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [scrollDirection, setScrollDirection] = useState(null);
+    const [hasPassedHero, setHasPassedHero] = useState(false);
+
+    useEffect(() => {
+        let lastScrollY = window.pageYOffset;
+
+        const updateScrollDirection = () => {
+            const scrollY = window.pageYOffset;
+            const direction = scrollY > lastScrollY ? "down" : "up";
+            if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+                setScrollDirection(direction);
+            }
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+        };
+
+        const updateHasPassedHero = () => {
+            const heroSection = document.getElementById("hero-section");
+            if (heroSection && window.pageYOffset >= heroSection.offsetTop + heroSection.offsetHeight) {
+                setHasPassedHero(true);
+            } else {
+                setHasPassedHero(false);
+            }
+        };
+
+        window.addEventListener("scroll", updateScrollDirection);
+        window.addEventListener("scroll", updateHasPassedHero);
+
+        return () => {
+            window.removeEventListener("scroll", updateScrollDirection);
+            window.removeEventListener("scroll", updateHasPassedHero);
+        };
+    }, [scrollDirection]);
+
     return (
-        <div className={`fixed inset-0 bg-transparent z-50 p-5 px-10 text-white font-primary flex h-32 justify-between items-center ${className}`}>
-            <img alt='logo' src={ImgLogo} className='w-15 h-fit shadow-lg' />
-            <div className='flex items-center gap-5'>
-                <div className='grid grid-cols-4 gap-1 text-center font-bold text-lg'>
+        <nav className={`${hasPassedHero ? "bg-white text-primary" : "bg-transparent text-white"} ${scrollDirection === "down" ? "-top-32" : "top-0"} transition-all duration-500 z-30 fixed inset-0 bg-transparent p-5 px-10 ${isOpen ? 'bg-white' : 'bg-transparent'} font-primary flex h-24 justify-between items-center ${className}`}>
+            <img
+                alt="logo"
+                src={ImgLogo}
+                className="w-16 md:w-24 h-auto shadow-lg"
+            />
+            <div className="hidden md:flex items-center gap-5">
+                <div className="grid grid-cols-4 gap-1 text-center text-lg">
                     <p>Beranda</p>
                     <p>Paket</p>
                     <p>Galeri</p>
@@ -17,6 +55,38 @@ export default function Navbar({ className }) {
                     <ButtonRounded text={`Daftar sekarang!`} />
                 </div>
             </div>
-        </div>
+            <div className={`transition-all md:hidden p-3 rounded-xl hover:border-primary hover:border-2 ${isOpen ? 'bg-primary' : 'bg-transparent'}`}>
+                <button
+                    className={`block hover:text-gray-200 focus:text-gray-200 focus:outline-none ${hasPassedHero ? "text-primary" : "text-white"} ${isOpen ? 'text-white' : ''}`}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
+                        <path
+                            className={`${isOpen ? 'hidden' : 'inline-flex'}`}
+                            fillRule='evenodd'
+                            clipRule='evenodd'
+                            d='M4 6h16v2H4V6zm0 5h16v2H4v-2zm16 5H4v2h16v-2z'
+                        />
+                        <path
+                            className={`${isOpen ? 'inline-flex' : 'hidden'}`}
+                            fillRule='evenodd'
+                            clipRule='evenodd'
+                            d='M5 4h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1zm5 6v2H6v-2h4zm0 4v2H6v-2h4zm6-4v2h-4v-2h4zm0 4v2h-4v-2h4z'
+                        />
+                    </svg>
+                </button>
+                <div className={`transition-all duration-700 ${isOpen ? 'block bg-white text-primary' : 'hidden'} absolute p-5 w-full top-full right-0 md:relative md:flex md:items-center md:gap-5 md:pt-5 md:pb-10 md:w-auto`}>
+                    <div className='grid grid-cols-1 gap-1 text-center text-lg'>
+                        <p>Beranda</p>
+                        <p>Paket</p>
+                        <p>Galeri</p>
+                        <p>Kontak</p>
+                    </div>
+                    <div className='pt-5 justify-center flex'>
+                        <ButtonRounded text={`Daftar sekarang!`} />
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
